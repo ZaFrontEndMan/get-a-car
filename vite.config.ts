f006382b349a6d70cd2274794775ecf7ced325b7
zafrontendman <128174502+ZaFrontEndMan@users.noreply.github.com>
2025-09-14 02:47:27 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  build: {
+    outDir: "dist",
   },
-}));
+});
